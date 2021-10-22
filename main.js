e33fd478b3a9b38d772534f6aa5634d7c75a155c
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-10-22 16:19:15 +0200

    il menu compare o scompare con un click

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ const hamb= document.querySelector("i.fa-bars");
 
 const menuVisible= document.querySelector("div.hamburger-menu");
 
+const cross= document.querySelector("i.fa-times");
+
 hamb.addEventListener('click',
 
-function() {
+function () {
     menuVisible.style.display="block";
+}
+)
+
+cross.addEventListener('click',
+
+function () {
+    menuVisible.style.display="none";
 }
 )
